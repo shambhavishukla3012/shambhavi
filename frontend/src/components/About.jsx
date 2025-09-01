b5e086ac82868f3fab180ad0cf7915cf135d9766
Shambhavi <shuklsha@iu.edu>
2025-08-31 20:02:13 -0400

    update image path

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
         <Row className="align-items-center">
           <Col lg={5} className="text-center mb-4 mb-lg-0">
             <img
-              src="/shambhavi/images/me.JPG"
+              src="/images/me.JPG"
               alt="Workspace"
               className="img-fluid"
               style={{
